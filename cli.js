f68fc17ffe2c2b0b34854d08552b2967d36bfcc6
Daniel Chiu <cycdaniel@gmail.com>
2019-10-04 01:42:22 -0500

    Setup initial files according to instructions

--- cli.js
+++ cli.js
@@ -1,20 +1,8 @@
 var axios = require("axios");
 var fs = require("fs");
+var keys = require("./keys.js")
 
-var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id: <your spotify client id>,
-  secret: <your spotify client secret>
-});
- 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
+
 
 
 function searchApi(query){
